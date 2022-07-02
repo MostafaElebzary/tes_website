8ab731b1192ac7e98b0d3000dcb0639f32ac3c64
MostafaElebzary <mostafaelebzary@gmail.com>
2022-07-02 14:52:30 +0200

    m

--- resources/views/front/pages/contact.blade.php
+++ resources/views/front/pages/contact.blade.php
@@ -11,13 +11,13 @@
         <div class="container">
             <div class="page-banner-content" data-aos="fade-right" data-aos-delay="50" data-aos-duration="500"
                  data-aos-once="true">
-                <h2>Contact</h2>
+                <h2>{{trans('lang.Contact')}}</h2>
 
                 <ul>
                     <li>
-                        <a href="index.html">Home</a>
+                        <a href="/">{{trans('lang.Home')}}</a>
                     </li>
-                    <li>Contact</li>
+                    <li>{{trans('lang.Contact')}}</li>
                 </ul>
             </div>
         </div>
@@ -90,8 +90,8 @@
     <!-- Start Map Area -->
     <div class="container ptb-100">
         <div class="map-location" id="map">
-{{--            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3455.0707789980365!2d31.330393414957108!3d30.00612398189629!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1458394aec1582c1%3A0x55ea8b2b038bfbfc!2sAl%20Mokatam%2C%20Al%20Abageyah%2C%20El%20Mokattam%2C%20Cairo%20Governorate!5e0!3m2!1sen!2seg!4v1654027100628!5m2!1sen!2seg" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>--}}
-       {!! settings_value('map_iframe') !!}
+            {{--            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3455.0707789980365!2d31.330393414957108!3d30.00612398189629!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1458394aec1582c1%3A0x55ea8b2b038bfbfc!2sAl%20Mokatam%2C%20Al%20Abageyah%2C%20El%20Mokattam%2C%20Cairo%20Governorate!5e0!3m2!1sen!2seg!4v1654027100628!5m2!1sen!2seg" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>--}}
+            {!! settings_value('map_iframe') !!}
         </div>
     </div>
     <!-- End Map Area -->
